a02fc9eb517e89b4637dce373cca3a7147edbda6
Vitaly Kulagin <vitaly.kulagin.85@bk.ru>
2020-10-09 09:22:05 +0300

    create project

---
+++ Objective_C_basic/Objective_C_basic/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective_C_basic
+//
+//  Created by Vit K on 09.10.2020.
+//  Copyright © 2020 Vit K. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
